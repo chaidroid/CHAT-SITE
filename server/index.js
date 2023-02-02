7fcbbadb8a903302d46f6f04e9362d1f3989de7b
chai <chaitanyagupta759@gmail.com>
2023-02-02 13:31:54 +0530

    server_URL_update

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT;
 const httpServer = http.createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: ["http://localhost:3000"],
+    origin: ["https://chai-chat-client.onrender.com"],
   },
 });
 
